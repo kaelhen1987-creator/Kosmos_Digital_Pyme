bd8bd9ff358e4525ca8ec0375f3d7646393b678e
kaelhen <henrry.alocilla@gmail.com>
2026-01-30 12:49:56 -0300

    Release: v0.11.0 (Stable)

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from app.ui.activation_view import build_activation_view
 from app.utils.helpers import show_message # Importar helper para mensajes
 
 # --- SYSTEM VERSION ---
-APP_VERSION = "0.11.0-dev"  # Bulk Sales Feature (Gramos/Pesos)
+APP_VERSION = "0.11.0"  # Bulk Sales & New Payment UI
 WIFI_MODE = False  # ACTIVAR PARA MODO WEB/WIFI (IPHONE/ANDROID)
 # ----------------------
 def main(page: ft.Page):
